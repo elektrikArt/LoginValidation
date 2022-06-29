a018b1d9daa8fe734d2d4e10c9f2c23dc9dbf51a
Scenester Hisako <sceneste@edu.21-school.ru>
2022-06-29 16:55:56 +0300

    Completed.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+int kitsCount = int.Parse(Console.ReadLine() ?? "_");
+
+for (int _ = 0; _ < kitsCount; _++)
+{
+    int attemptsCount = int.Parse(Console.ReadLine() ?? "_");
+
+    var validLogins = new List<string>();
+    for (int __ = 0; __ < attemptsCount; __++)
+    {
+        string attempt = Console.ReadLine() ?? "_";
+        if (IsValid(attempt, validLogins))
+        {
+            Console.WriteLine("YES");
+            validLogins.Add(attempt.ToLower());
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+
+    Console.WriteLine();
+}
+
+static bool IsValid(string attempt, List<string> validLogins)
+{
+    if (    attempt.All(c => char.IsAscii(c) && (char.IsLetterOrDigit(c) || c == '_' || c == '-'))
+        &&  attempt[0] != '-'
+        &&  attempt.Length >= 2
+        &&  attempt.Length <= 24
+        &&  validLogins.Contains(attempt.ToLower()) == false)
+    {
+        return true;
+    }
+    return false;
+}
